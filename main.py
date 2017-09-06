b702ff6a099ac887810e6a3159bd939468298aa1
William <nettee.liu@gmail.com>
2017-09-07 00:13:58 +0800

    print log info

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import qsbk
 import lily
 import sendmail
 
+receivers = [
+    'anchori@163.com',
+    #'nettee.liu@gmail.com',
+    #'tfn0510@gmail.com',
+]
+
 active_packages = [
     qsbk,
     lily,
@@ -18,16 +24,25 @@ if __name__ == '__main__':
     today = date.today().strftime('%Y%m%d')
 
     for package in active_packages:
+        print('Fetching {}...'.format(package.abbr))
         fp = Path('data/{}-{}.json'.format(package.abbr, today))
         if fp.is_file():
+            print('Reading from file {}...'.format(str(fp)))
             with fp.open('r') as f:
                 json_string = f.read()
                 data = json.loads(json_string)
         else:
+            print('Crawler working...')
             data = package.collect()
             json_string = json.dumps(data)
             with fp.open('w') as f:
                 print(json_string, file=f)
+            print('Saved to file {}'.format(str(fp)))
 
         mail = package.generate_mail(data, today)
-        sendmail.sendmail(mail)
+        for receiver in receivers:
+            try:
+                sendmail.sendmail(mail, receiver)
+                print('Sent {} to {}'.format(package.abbr, receiver))
+            except Exception:
+                print('Error in sending {} to {}'.format(package.abbr, receiver))
--- sendmail.py
+++ sendmail.py
@@ -10,12 +10,6 @@ from email.utils import parseaddr, formataddr
 
 import smtplib
 
-receivers = [
-    'anchori@163.com',
-    #'nettee.liu@gmail.com',
-    #'tfn0510@gmail.com',
-]
-
 config = configparser.ConfigParser()
 config.read('mail.ini')
 from_name = '大萌萌新鲜推送'
@@ -26,19 +20,17 @@ smtp_server = config['sender']['server']
 def format_addr(name, addr):
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
-def sendmail(mail):
+def sendmail(mail, receiver):
     mimetype = mail['mimetype']
     subject = mail['subject']
     body = mail['body']
 
-    for receiver in receivers:
-        msg = MIMEMultipart()
-        msg['From'] = format_addr(from_name, from_addr)
-        msg['To'] = receiver
-        msg['Subject'] = Header(subject, 'utf-8').encode()
-        msg.attach(MIMEText(body, mimetype, 'utf-8'))
-        server = smtplib.SMTP(smtp_server, 25)
-        server.set_debuglevel(1)
-        server.login(from_addr, password)
-        server.sendmail(from_addr, [receiver], msg.as_string())
-        server.quit()
+    msg = MIMEMultipart()
+    msg['From'] = format_addr(from_name, from_addr)
+    msg['To'] = receiver
+    msg['Subject'] = Header(subject, 'utf-8').encode()
+    msg.attach(MIMEText(body, mimetype, 'utf-8'))
+    server = smtplib.SMTP(smtp_server, 25)
+    server.login(from_addr, password)
+    server.sendmail(from_addr, [receiver], msg.as_string())
+    server.quit()
